c7949c721276b7574dfeaa358667e8ddde2fd62c
sadra <sadra3430452821@gmail.com>
2025-11-09 19:18:31 +1000

    idkk

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+// next.config.js (CommonJS)
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'myportfoilo'
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/myportfoilo',
-};
-export default nextConfig;
+  output: 'export',                  // جایگزین next export
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  images: { unoptimized: true },     // چون GitHub Pages بهینه‌ساز تصویر ندارد
+  trailingSlash: true,               // از 404 روی مسیرها جلوگیری می‌کند
+}
+
+module.exports = nextConfig
